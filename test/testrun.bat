a976e5cd8e25e64e8662218d0f7f6fb2b2ef4824
Toby Allen <toby@toflidium.com>
2014-08-12 23:47:18 +0100

    Additional Tests
    Test to check single file and single file with output dir

--- test/testrun.bat
+++ test/testrun.bat
@@ -9,10 +9,17 @@ REM %~d0 and %~p0 together give the full directory this batch file is executing 
 
 
 REM Individually try each format on Test Document
-REM FOR /F "eol=; tokens=1,2* delims=, " %%i in (testdata.txt) do "../exe/docto.exe"  -f "%~d0%~p0\Inputfiles\pie3.doc"  -o "%~d0%~p0GeneratedFiles\pie3out_%%i.%%j"  -T  %%i
+FOR /F "eol=; tokens=1,2* delims=, " %%i in (testdata.txt) do "../exe/docto.exe"  -f "%~d0%~p0\Inputfiles\pie3.doc"  -o "%~d0%~p0GeneratedFiles\pie3out_%%i.%%j"  -T  %%i
 
 REM Try on Directory
 "../exe/docto.exe"  -f "%~d0%~p0Inputfiles\"  -o "%~d0%~p0GeneratedFiles"    -T  wdFormatPDF -OX pdf
+
+REM Try on Single
+"../exe/docto.exe"  -f "%~d0%~p0Inputfiles\pie3.doc"  -o "%~d0%~p0GeneratedFiles\Pie3Single.pdf"    -T  wdFormatPDF
+
+REM Try on Single no output file
+"../exe/docto.exe"  -f "%~d0%~p0Inputfiles\pie3.doc"  -o "%~d0%~p0GeneratedFiles\Single"    -T  wdFormatPDF 
+
 
 REM Should produce an error
 "../exe/docto.exe"  -f "%~d0%~p0Inputfiles\pie3.doc"  -o "%~d0%~p0GeneratedFiles\Pie3.pdf"    -T  wdFormatTestPDF
