bb6549a87cbfa4f1b787550a16ab52b0a051572f
Toby Allen <toby@toflidium.com>
2014-11-15 20:31:07 +0000

    Updates to get xlsto working

--- test/testxlsrun.bat
+++ test/testxlsrun.bat
@@ -2,3 +2,5 @@ REM XLSTO
 
 REM Try on Directory
 "../exe/xlsto.exe"  -f "%~d0%~p0Inputfiles\"  -o "%~d0%~p0GeneratedFiles"    -T  xlPDF -OX .pdf
+REM convert to csv
+"../exe/xlsto.exe"  -f "%~d0%~p0Inputfiles\"  -o "%~d0%~p0GeneratedFiles"    -T  xlcsv -OX .csv
