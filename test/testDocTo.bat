9cd74392bc71a2aa13b236d8f44ef20c7c5487c4
Toby Allen <toby@toflidium.com>
2016-05-16 20:55:04 +0100

    test without error *****

--- test/testDocTo.bat
+++ test/testDocTo.bat
@@ -12,19 +12,19 @@ REM Output Help Text
 "../exe/docto.exe" -h
 
 
-REM *************************************************
+REM ---------------------------------
 REM Main Test : 
 REM Loop through each format provided in doctoFormatList and try to convert our
 REM test file to each format.
-REM *************************************************
+REM ---------------------------------
 
 FOR /F "eol=; tokens=1,2* delims=, " %%i in (doctoFormatList.txt) do "../exe/docto.exe"  -f "%~d0%~p0\Inputfiles\pie3.doc"  -o "%~d0%~p0GeneratedFiles\pie3out_%%i.%%j"  -T  %%i
 
 
 
-REM *********************************************
+REM ---------------------------------
 REM Convert All files in Input Directory to PDF 
-REM *********************************************
+REM ---------------------------------
 
 "../exe/docto.exe"  -f "%~d0%~p0Inputfiles\"  -o "%~d0%~p0GeneratedFiles"    -T  wdFormatPDF -OX pdf
 
@@ -47,9 +47,9 @@ REM Should produce an error
 "../exe/docto.exe"  -f "%~d0%~p0Inputfiles\pie3_doesntexist.doc"  -o "%~d0%~p0GeneratedFiles\Pie3.pdf"    -T  wdFormatPDF
 
 REM Test Webhook
-REM *********************************
+REM ---------------------------------
 REM To view visit http://toflidium.com/webhooks/docto/docto_test_values.txt
-REM *********************************
+REM ---------------------------------
 "../exe/docto.exe"  -f "%~d0%~p0Inputfiles\pie3.doc"  -o "%~d0%~p0GeneratedFiles\Pie3.pdf"    -T  wdFormatPDF -W http://toflidium.com/webhooks/docto/webhook_test.php
 
 
