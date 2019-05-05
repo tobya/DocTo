b78dbde574949fbd36989ef4b699cc7014ba8648
Toby Allen <tobyallen@toflidium.com>
2019-05-05 21:51:32 +0100

    updtes

---
+++ test/testxlsrun_protect.bat
@@ -0,0 +1,11 @@
+REM XLSTO
+
+
+
+REM Full Dir
+REM  "../exe/xlsto.exe" -L 10  -f "%~d0%~p0Inputfiles\"  -o "%~d0%~p0GeneratedFiles"    -T  xlExcel9795 -OX .xls
+
+
+  "../exe/xlsto.exe" -L 10  -f "%~d0%~p0Inputfilesexcel\Week 1 Test.xls"   -o "%~d0%~p0GeneratedFiles"  --ExcelProtect   -T  xlExcel8 -OX .xls
+REM "../exe/xlsto.exe" -L 10  -f "%~d0%~p0Inputfilesexcel\Week 1 Test.xls"   -o "%~d0%~p0GeneratedFiles" --ExcelProtect   -T  xlExcel9795 -OX .xls
+
