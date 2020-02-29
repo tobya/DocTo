e81703222498be6d6cf8dae3b466547aad463d72
Toby Allen <tobyallen@toflidium.com>
2020-02-29 09:15:13 +0000

    test

---
+++ test/pptestrelative.bat
@@ -0,0 +1,4 @@
+REM If output Dir left out default to input
+REM copy  ".\Inputfiles\pie3.doc"  ".\GeneratedFiles\PieNoOutputTest.doc"
+REM Try on Single
+"../exe/docto.exe" -PP -f ".\Inputfilespp\Presentation1.ppt"  -o ".\GeneratedFiles\res3.jpg"    -T  17 -l 10
