e6901d16d8e81ebb625b4f3c6358e68ca17bdadb
Toby Allen <tobyallen@toflidium.com>
2020-06-20 18:15:38 +0100

    update testfiles

--- test/testxls_relative.bat
+++ test/testxls_relative.bat
@@ -1,2 +1,2 @@
  REM Load xls from a relative path and convert to csv
-"../exe/docto.exe" -XL  -f "inputfilesexcel\Week 1 Test.xls"  -o "GeneratedFiles\Week1.csv"    -T  xlCSV -l 10
+"../exe/docto.exe" -XL  -f "inputfilesxl\Week 1 Test.xls"  -o "GeneratedFiles\Week1.csv"    -T  xlCSV -l 10
