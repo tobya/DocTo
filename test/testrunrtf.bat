670bb94af34667df42242094d9c5e7eec90a0301
Toby Allen <tobyallen@toflidium.com>
2020-11-07 18:05:14 +0000

    renamed folder

--- test/testrunrtf.bat
+++ test/testrunrtf.bat
@@ -1,5 +1,5 @@
 REM Try on Directory with rtf files
-"../exe/docto.exe"  -f "%~d0%~p0Inputfiles\rtf" -fx .rtf -o "%~d0%~p0GeneratedFiles"    -T  wdFormatPDF 
+"../exe/docto.exe"  -f "%~d0%~p0Inputfiles\others" -fx .rtf -o "%~d0%~p0GeneratedFiles"    -T  wdFormatPDF 
 
 
 
