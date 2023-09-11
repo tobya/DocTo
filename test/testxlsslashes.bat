82fe3db28adbc072e8a3caf626f6322a19c2203f
Toby Allen <toby@cookingisfun.ie>
2023-09-11 17:50:19 +0100

    extend tests

--- test/testxlsslashes.bat
+++ test/testxlsslashes.bat
@@ -2,4 +2,4 @@ REM XLSTO
 
 
 REM Try on Single no output file with Verbose Logging
- "../exe/xlsto.exe" -L 10 -f "%~d0%~p0Inputfiles"  -o "%~d0%~p0GeneratedFiles\testcreatedir\"    -T  xlcsv 
+ "../exe/docto.exe" -XL -L 10 -f "%~d0%~p0Inputfiles"  -o "%~d0%~p0GeneratedFiles\testcreatedir\"    -T  xlcsv 
