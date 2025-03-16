77eb833284c2099c887f2939a57bb5098ae57c16
Toby Allen <toby@cookingisfun.ie>
2025-03-16 07:55:07 +0000

    pwerpoint test

---
+++ test/pptesdir.bat
@@ -0,0 +1,5 @@
+REM If output Dir left out default to input
+REM copy  ".\Inputfiles\pie3.doc"  ".\GeneratedFiles\PieNoOutputTest.doc"
+REM Try on Single
+
+"../exe/32/docto.exe" -PP -f ".\Inputfilespp\"  -o ".\GeneratedFiles\"  -ox ".pdf"  -T  ppSaveAsPDF -l 10
