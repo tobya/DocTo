0bae300e2412bf1d13fe733df53ea0b6453ba900
Toby Allen <toby@cookingisfun.ie>
2025-11-19 09:24:30 +0000

    note

--- test/testxlspass.bat
+++ test/testxlspass.bat
@@ -1,2 +1,3 @@
  REM Load csv files convert to xls
-"../exe/32/docto.exe" -XL  -f "C:\dev\github\docto\test\inputfilesxl\Book1 with password.xls"  -o "C:\dev\github\docto\test\GeneratedFiles"    -T  xlpdf -l 10
+REM will skip password protected file
+"../exe/32/docto.exe" -XL  -f "C:\development\github\docto\test\inputfilesxl\Book1 with password.xls"  -o "C:\development\github\docto\test\GeneratedFiles"    -T  xlpdf -l 10
