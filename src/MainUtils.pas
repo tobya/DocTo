60214c4c65b81398704dd57db4d5d8dddb51c111
Toby Allen <toby@cookingisfun.ie>
2026-05-31 11:47:02 +0100

    recurse

--- src/MainUtils.pas
+++ src/MainUtils.pas
@@ -308,7 +308,8 @@ type
 
 implementation
 
-uses baseConfig, ConfigOutput, ConfigInput, configLogLevel, configFormat;
+uses baseConfig, ConfigOutput, ConfigInput, configLogLevel, configFormat,      configNoRecurse
+;
 
 { TConsoleLog }
 
@@ -1728,6 +1729,7 @@ begin
   TParamOutputExtension.RegisterParameters(Result);
   TParamLogLevel.RegisterParameters(Result);
   TParamFormat.RegisterParameters(Result);
+  TParamNoRecurse.RegisterParameters(Result);
 
 end;
 
