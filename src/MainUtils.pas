bcb296944350cdfb48bfe7df32106623aee3f821
Toby Allen <toby@cookingisfun.ie>
2026-06-01 08:31:03 +0100

    version set

--- src/MainUtils.pas
+++ src/MainUtils.pas
@@ -33,7 +33,7 @@ Const
   MSVISIO = 4;
 
   
-  DOCTO_VERSION = '1.16.5';  // dont use 0x - choco needs incrementing versions.
+  DOCTO_VERSION = '1.16.6';  // dont use 0x - choco needs incrementing versions.
   DOCTO_VERSION_NOTE = ' x64 Release ';
 type
 
